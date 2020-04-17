d396f4fe0651a41a3df1ef93ec5fdba8a76aeeef
ademola17 <63821635+ademola17@users.noreply.github.com>
2020-04-17 19:21:26 +0100

    My First  Commit
    There's still more to be updated

---
+++ submit.php
@@ -0,0 +1,32 @@
+ <?php
+
+$host="localhost";
+$user="root";
+$password="";
+
+
+
+
+
+
+  ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>StreamsYjd</title>
+ </head>
+ <body>
+ 	<div class="container">
+ 		<img src="">
+ 		<div class="form_input">
+ 			<input type="text" name="username" placeholder="Enter your name"/>
+ 		</div>
+ 		<div class="form_input">
+ 			<input type="password" name="password" placeholder="password">
+ 		</div>
+ 		<input type="submit" name="submit" value="LOGIN" class="btn-login">
+ 		
+ 	</div>
+ 
+ </body>
+ </html>
